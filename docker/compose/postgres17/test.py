d10d6315544f9f95ce87b24b49768ff0a42c260e
James Kelly <jamesdkelly88@outlook.com>
2024-09-04 19:51:11 +0100

    v1.2.0 - adding postgres17

---
+++ docker/compose/postgres17/test.py
@@ -0,0 +1,21 @@
+import psycopg2
+import os
+
+password = os.environ['SECRET_PASSWORD']
+host = os.environ['DOMAIN']
+port = 5431
+
+try:
+    conn = psycopg2.connect(f"dbname='postgres' user='postgres' host='{host}' port={port} password='{password}'")
+except:
+    print("I am unable to connect to the database")
+    quit()
+
+with conn.cursor() as curs:
+
+    try:
+        curs.execute("SELECT version()")
+        single_row = curs.fetchone()
+        print(f"{single_row}")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
