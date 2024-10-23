c3e9014e047cde1bc0362f44e873e482cfd4116e
jamesdkelly88 <47245317+jamesdkelly88@users.noreply.github.com>
2024-10-23 19:47:43 +0100

    Microsoft SQL Server (#70)

---
+++ docker/compose/mssql2022/test.py
@@ -0,0 +1,21 @@
+import pyodbc
+import os
+
+password = os.environ['SECRET_PASSWORD']
+host = os.environ['DOMAIN']
+port = 1433
+username = "sa"
+
+cnxn = pyodbc.connect("Driver={ODBC Driver 18 for SQL Server};"
+                      f"Server={host},{port};"
+                      "Database=master;"
+                      f"UID={username};"
+                      f"PWD={password};"
+                      "TrustServerCertificate=yes;")
+
+
+cursor = cnxn.cursor()
+cursor.execute('SELECT @@VERSION AS Version')
+
+for row in cursor:
+    print(row[0])
