74446625b508c55d32913fef0aeacde5e1c18d14
James Kelly <jamesdkelly88@outlook.com>
2024-11-27 19:19:28 +0000

    python venv working

--- shell.nix
+++ shell.nix
@@ -1,18 +1,25 @@
 let
   unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
 in
-{ nixpkgs ? import <nixpkgs> {} }:
-with nixpkgs; mkShell {
-  packages = with pkgs; [
-    awscli2
-    go-task
-    powershell
-    sqlcmd
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell rec {
+
+  buildInputs = [
+    pkgs.awscli2
+    pkgs.go-task
+    pkgs.powershell
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.sqlcmd
+    pkgs.zlib
     unstable.pkgs.postgresql_17
   ];
+
+  # docker should be running on the system already, so not included here!
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+  '';
 }
-
-# docker should be running on the system already, so not included here!
-
-# TODO:
-# install python (with pip and venv)
