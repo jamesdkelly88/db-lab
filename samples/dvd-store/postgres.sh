af5b829216131cd840661c3b8bbfd1205f072ae5
James Kelly <jamesdkelly88@outlook.com>
2025-01-05 12:44:34 +0000

    finished pagila script

--- samples/dvd-store/postgres.sh
+++ samples/dvd-store/postgres.sh
@@ -1,10 +1,12 @@
 set -e
 
+source ../../db-lab.env
+
 case $1 in
     12 | 13 | 14 | 15 | 16 | 17 )
         PG_HOSTNAME=postgres$1;;    
     ha )
-        PG_HOSTNAME=pg_proxy;;
+        PG_HOSTNAME=pg-proxy;;
     *)
         echo "usage: ./postgres.sh <version>";
         echo "examples:";
@@ -18,19 +20,69 @@ REPO_URL=https://github.com/devrimgunduz/pagila.git
 VOLUME_NAME="${DB_NAME}_${RANDOM}"
 
 echo "Creating docker volume: $VOLUME_NAME"
-# create docker volume
-
-echo "Cloning repository: $REPO_URL"
-# alpine/git clone repo to volume
+docker volume create $VOLUME_NAME
 
 echo "Creating database: $DB_NAME on target: $PG_HOSTNAME" 
-# alpine/psql psql -c "CREATE DATABASE pagila;" (no volume mount)
+docker run \
+--attach stdout \
+--attach stderr \
+--env PGHOST=$PG_HOSTNAME \
+--env PGPORT=5432 \
+--env PGDATABASE=postgres \
+--env PGUSER=postgres \
+--env PGPASSWORD=$SECRET_PASSWORD \
+--name pagila-create \
+--network postgres \
+--rm \
+alpine/psql \
+-c "CREATE DATABASE $DB_NAME;"
+
+echo "Cloning repository: $REPO_URL"
+docker run \
+--attach stdout \
+--attach stderr \
+--volume $VOLUME_NAME:/data \
+--name pagila-clone \
+--network postgres \
+--rm \
+--workdir /data \
+alpine/git \
+clone $REPO_URL .
 
 echo "Applying database schema"
-# alpine/psql psql -f sample/pagila-schema.sql 
+docker run \
+--attach stdout \
+--attach stderr \
+--env PGHOST=$PG_HOSTNAME \
+--env PGPORT=5432 \
+--env PGDATABASE=$DB_NAME \
+--env PGUSER=postgres \
+--env PGPASSWORD=$SECRET_PASSWORD \
+--volume $VOLUME_NAME:/data \
+--name pagila-schema \
+--network postgres \
+--rm \
+--workdir /data \
+alpine/psql \
+-f pagila-schema.sql
 
 echo "Loading data"
-# alpine/psql psql -f sample/pagila-insert-data.sql
+docker run \
+--attach stdout \
+--attach stderr \
+--env PGHOST=$PG_HOSTNAME \
+--env PGPORT=5432 \
+--env PGDATABASE=$DB_NAME \
+--env PGUSER=postgres \
+--env PGPASSWORD=$SECRET_PASSWORD \
+--volume $VOLUME_NAME:/data \
+--name pagila-data \
+--network postgres \
+--rm \
+--workdir /data \
+alpine/psql \
+-q -1 -f pagila-insert-data.sql
 
-echo "Cleaning up"
+echo "Cleaning up volume $VOLUME_NAME"
 # delete docker volume
+docker volume rm $VOLUME_NAME
