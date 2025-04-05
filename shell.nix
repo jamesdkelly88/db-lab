da496451c733d7678609edb0c9a0d1af9e16244d
jamesdkelly88 <47245317+jamesdkelly88@users.noreply.github.com>
2025-04-05 18:55:57 +0100

    Fixing nix shell for ODBC drivers (#221)

--- shell.nix
+++ shell.nix
@@ -15,16 +15,23 @@ pkgs.mkShell rec {
     pkgs.python311
     pkgs.python311Packages.pip
     pkgs.sqlcmd
-    pkgs.unixODBC
-    pkgs.unixODBCDrivers.msodbcsql18
     pkgs.zlib
     unstable.pkgs.postgresql_17
   ];
 
   # docker should be running on the system already, so not included here!
 
+  # ODBC drivers for SQL Server required - I can't get this to work in shell.nix so the following needs adding at the NixOS level:
+  # packages:
+  # - unixODBC
+  # - unixODBCDrivers.msodbcsql18
+  #
+  # config:
+  # environment.unixODBCDrivers = with pkgs.unixODBCDrivers; [ msodbcsql18 ];
+
   shellHook = ''
     export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.unixODBC}/lib:$LD_LIBRARY_PATH"
   '';
 }
