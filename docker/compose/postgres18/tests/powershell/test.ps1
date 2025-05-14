1fff13ee7520983176b83631940a3d403f323b17
James Kelly <jamesdkelly88@outlook.com>
2025-05-14 18:53:12 +0100

    adding postgres18

---
+++ docker/compose/postgres18/tests/powershell/test.ps1
@@ -0,0 +1,12 @@
+Install-Module PSPostgres
+Import-Module PSPostgres
+
+$hostname            = $env:DOMAIN
+$port                = 54328
+$username            = "postgres"
+$password            = $env:SECRET_PASSWORD
+
+$connection_string   = "Server=$($hostname);Port=$($port);Database=postgres;User Id=$($username);Password=$($password)"
+$connection          = Connect-Postgres -ConnectionString $connection_string
+
+Invoke-PostgresQuery -Connection $connection -Query "SELECT version();"
